1f38da5dc7b032c08e45b8952a47ec6c6cf80225
mdshakil04 <m.shakil04@yahoo.com>
2023-11-28 22:27:20 +0600

    All Classes components functionality added

---
+++ src/Pages/AllClasses/AllClass.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AllClass = ({ allClass }) => {
+  const { title, name, image, price, short_description, total_enrollment } =
+    allClass;
+  return (
+    <div className="card  bg-base-100 shadow-xl">
+      <figure>
+        <img
+            className=" lg:h-[350px] w-full"
+          src={image}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-2xl font-bold">
+          Instructor : {title}
+        </h2>
+        <h2 className="card-title">
+          Instructor : {name}
+        </h2>
+        <p>{short_description}</p>
+        <h2>Price: ${price}</h2>
+        <div className="card-actions justify-start">
+          <div className="badge badge-outline">Total Enrolment: {total_enrollment}</div>
+        </div>
+        <div className="card-actions justify-center">
+          <button className="btn btn-outline btn-primary">Enroll Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllClass;
--- src/Pages/AllClasses/AllClasses.jsx
+++ src/Pages/AllClasses/AllClasses.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../Components/SectionTitle/SectionTitle';
+import AllClass from './AllClass';
 
 const AllClasses = () => {
+    const [allClass, setAllClass] = useState([]);
+    useEffect(() =>{
+        fetch("http://localhost:5000/class")
+        .then(res => res.json())
+        .then(data => setAllClass(data))
+    },[]);
+    
     return (
         <div className=' pt-24'>
             <SectionTitle
                 heading={'Our all Classes'}
             >
             </SectionTitle>
+            <div className=' container mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-4'>
+                {
+                    allClass.map((allClass) => <AllClass
+                        key={allClass._id}
+                        allClass={allClass}
+                    ></AllClass>)
+                }
+            </div>
         </div>
     );
 };
